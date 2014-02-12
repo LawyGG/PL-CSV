c3a45f17c3d25095d114083e87c457038e5ebbf1
alu0100698244 <alu0100698244@ull.edu.es>
2014-02-12 13:42:47 +0000

    Mejoras en la exp regular
    Añadidos los numeros decimales (x.x)

--- csv.js
+++ csv.js
@@ -10,8 +10,8 @@ function calculate() {
   var result;
   var original       = document.getElementById("original");
   var temp = original.value;
-  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|([0-9]+\s*)|(\s+,),?/g;
-  //palabras entrecomilladas, numeros sin comillas y campos en blanco <, ,>
+  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|([0-9]+(:?\.[0-9]+)*\s*),?|(\s+,),?/g;
+  //palabras entrecomilladas, numeros sin comillas(opcionalmente decimales x.x) y campos en blanco <, ,>
   var lines = temp.split(/\n+\s*/);
   var commonLength = NaN;
   var r = [];
