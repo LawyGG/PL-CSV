9d43fef23d34bb296a19ae75930c9e8fe65ea643
alu0100698244 <alu0100698244@ull.edu.es>
2014-02-12 13:49:27 +0000

    Exp regular Final

--- csv.js
+++ csv.js
@@ -10,7 +10,7 @@ function calculate() {
   var result;
   var original       = document.getElementById("original");
   var temp = original.value;
-  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|([0-9]+(:?\.[0-9]+)*\s*),?|(\s+,),?/g;
+  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|([0-9]+(?:\.[0-9]+)*\s*),?|(\s+,),?/g;
   //palabras entrecomilladas, numeros sin comillas(opcionalmente decimales x.x) y campos en blanco <, ,>
   var lines = temp.split(/\n+\s*/);
   var commonLength = NaN;
